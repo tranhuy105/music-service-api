ccbcc9f818ad9d4af6968fdc19062c237506e7d6
tranhuy105 <imhuy105@gmail.com>
2024-07-25 15:46:17 +0700

    first commit

--- src/main/java/com/tranhuy105/musicserviceapi/MusicServiceApiApplication.java
+++ src/main/java/com/tranhuy105/musicserviceapi/MusicServiceApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusicServiceApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusicServiceApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MusicServiceApiApplication.class, args);
+    }
 
 }
--- src/test/java/com/tranhuy105/musicserviceapi/MusicServiceApiApplicationTests.java
+++ src/test/java/com/tranhuy105/musicserviceapi/MusicServiceApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MusicServiceApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
