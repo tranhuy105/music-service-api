279436d491d3fe5705e8b921a9a8a325d5150ece
tranhuy105 <imhuy105@gmail.com>
2024-08-16 23:07:50 +0700

    feat: Add endpoints for updating and deleting artist profiles
    - Implemented PUT /api/artists/{id} for updating artist profiles with genre management.
    - Added DELETE /api/artists/{id} for deleting artist profiles.

--- src/main/java/com/tranhuy105/musicserviceapi/controller/ArtistController.java
+++ src/main/java/com/tranhuy105/musicserviceapi/controller/ArtistController.java
@@ -1,10 +1,13 @@
 package com.tranhuy105.musicserviceapi.controller;
 
 
+import com.tranhuy105.musicserviceapi.dto.UpdateArtistProfileRequestDto;
 import com.tranhuy105.musicserviceapi.model.*;
 import com.tranhuy105.musicserviceapi.service.ArtistService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,5 +40,21 @@ public class ArtistController {
     @GetMapping("/{id}/top-tracks")
     public ResponseEntity<List<TrackDetail>> findTopTrackByArtistId(@PathVariable Long id) {
         return ResponseEntity.ok(artistService.findTopTrack(id));
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ARTIST') || hasRole('ROLE_MANAGER')")
+    public ResponseEntity<String> updateArtistProfile(
+            @PathVariable Long id,
+            @RequestBody @Valid UpdateArtistProfileRequestDto dto) {
+        artistService.updateArtistProfile(id, dto);
+        return ResponseEntity.ok("Artist profile updated successfully.");
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<String> deleteArtistProfile(@PathVariable Long id) {
+        artistService.deleteArtistProfile(id);
+        return ResponseEntity.ok("Artist profile deleted successfully.");
     }
 }
